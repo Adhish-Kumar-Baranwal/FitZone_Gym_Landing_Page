2b5816b0535565a683508108c822f7b6153e3994
Adhish-Kumar-Baranwal <adhish0725@gmail.com>
2026-09-09 21:51:36 +0530

    update email transport configuration and recipient details in sendMail utility

--- gym-backend/src/utils/sendMail.js
+++ gym-backend/src/utils/sendMail.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 const sendMail = async ({ to, subject, text }) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+    host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
     auth: {
@@ -14,8 +14,8 @@ const sendMail = async ({ to, subject, text }) => {
   await transporter.verify();
 
   await transporter.sendMail({
-    from: process.env.GYM_OWNER_MAIL_ID,
-    to,
+    from: process.env.GYM_OWNER_MAIL_ID_REAL,
+    to: process.env.GYM_OWNER_MAIL_ID_ALIAS,
     subject,
     text,
   });
