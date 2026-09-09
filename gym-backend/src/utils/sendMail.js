27a27f9ff13dd77ffb4b742c28c3ecf0616f386a
Adhish-Kumar-Baranwal <adhish0725@gmail.com>
2026-09-10 00:09:30 +0530

    fix: update SMTP port in sendMail function for correct email delivery

--- gym-backend/src/utils/sendMail.js
+++ gym-backend/src/utils/sendMail.js
@@ -5,7 +5,7 @@ const sendMail = async ({ to, subject, text }) => {
 
   const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 2525,
     secure: false,
     auth: {
       user: process.env.GYM_OWNER_MAIL_ID,
